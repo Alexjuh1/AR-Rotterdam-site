bcf5eca5817eb9ca3a28ddbf89ccbe2fc55de169
OmniscientJV <j.vpersonal@hotmail.com>
2016-04-05 20:04:50 +0200

    -- Settings file.

---
+++ backend/includes/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JV
+ * Date: 4-4-2016
+ * Time: 09:58
+ */
+define('PLACES_API_KEY', ''); // Removed, add it yourself.
+define('ROTTERDAM_POINT', '51.9139567,4.5041676');
+define('LOCATION_RADIUS', 5000);
+
+class Settings
+{
+    static $placesID = [
+        'erasmusbrug' => 'ChIJW08dqWYzxEcRWFwW9N9RO_Q',
+    ];
+}
